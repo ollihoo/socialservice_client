7740bbfc61ea7bfd889a8bafba9261519089bfc5
Oliver Hoogvliet <oliver@hoogvliet.de>
2025-05-09 09:47:45 +0200

    Remove unused code from utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,3 @@
-import { Revenue } from './definitions';
-
 export function createUrl(endpoint: string): string {
   return 'http://' + process.env.BACKEND_HOST + ':' + process.env.BACKEND_PORT + endpoint;
 }
