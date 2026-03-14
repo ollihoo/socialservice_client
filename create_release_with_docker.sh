3618d87161d3db479b3327ea584d0dfe452f78fb
Oliver Hoogvliet <oliver@hoogvliet.de>
2026-03-14 10:35:58 +0100

    Configure docker deployment explicitly to enable tests. Use feature in githb actions

--- create_release_with_docker.sh
+++ create_release_with_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" = "deploy" ]; then
+  DEPLOY=true
+fi
+
 if [ -f .env ]; then
     source .env
 else
@@ -8,12 +12,33 @@ else
 fi
 
 starttime=$(date +%s)
-
 IMAGE_TAG=$(date +%y%m%d%H%M)
 
-docker buildx build --platform linux/amd64 -t ${DOCKER_USER}/socialservices_frontend:$IMAGE_TAG . && \
-docker push ${DOCKER_USER}/socialservices_frontend:${IMAGE_TAG}
+docker buildx build --platform linux/amd64 -t ${DOCKER_USER}/socialservices_frontend:$IMAGE_TAG .
 
-endtime=$(date +%s)
-delta=$((endtime - starttime))
-echo "Ok. Duration: $delta s"
+if [ $? -eq 0 ]; then
+    echo "✅ docker build successful"
+else
+    echo "❌ docker build failed"
+    exit 1
+fi
+
+if [ "$DEPLOY" = "true" ]; then
+  docker push ${DOCKER_USER}/socialservices_frontend:${IMAGE_TAG}
+
+ if [ $? -eq 0 ]; then
+       endtime=$(date +%s)
+       delta=$((endtime - starttime))
+       echo "Duration: $delta seconds"
+       echo "✅ Ok. Latest IMAGE: ${DOCKER_USER}/${APP_NAME}:${IMAGE_TAG}"
+       exit 0
+   else
+       echo "❌ docker push failed"
+       exit 1
+   fi
+else
+  endtime=$(date +%s)
+  delta=$((endtime - starttime))
+  echo "File hasn't been pushed to docker hub. Duration: $delta s"
+  exit 1
+fi
